906db2e3b379416f2a92cf6f7e1e3430e3958e9d
Gustavo Larsen <guslarsen@gmail.com>
2022-06-26 16:09:20 -0300

    Add integracao com meio de pagamento Stripe

--- src/components/SubscribeButton/Subscribe.component.tsx
+++ src/components/SubscribeButton/Subscribe.component.tsx
@@ -1,6 +1,8 @@
 import styles from './SubscribeButton.module.scss';
-
-const SubscribeButton = () => {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   return (
     <button className={styles.subscribeButton} type="button">
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton/Subscribe.component';
+import { stripe } from '../services/stripe';
 import styles from '../styles/Home.module.scss';
 
-const Home = () => {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+const Home = ({ product }: HomeProps) => {
   return (
     <>
       <Head>
@@ -17,16 +25,36 @@ const Home = () => {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LF08wGMbVOYsYjdBelENbrD', {
+    expand: ['product'],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount! / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
 };
 
 export default Home;
